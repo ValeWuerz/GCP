bdbdcc1771a41c0a199a8d15793dc01a54afc039
valentin.wuerz <valentin.wuerz@siemens.com>
2021-10-13 13:46:05 +0200

    correct time calc

--- testing.js
+++ testing.js
@@ -1,14 +1,24 @@
+let zeitsample1= '100629';
+let zeitsample2= '075900'
 
-let current_state= [1,1,0]
+ 
 
-console.log(calc_num(current_state));
 
-function calc_num(current_state) {
-    let sum=0
-    for (let i = 0; i < current_state.length; i++) {
-        sum += current_state[i];
-    }
-    return sum
+let end = zeit(zeitsample1)
+let start = zeit(zeitsample2)
+
+let difference = end.getTime() - start.getTime()
+let minutes = Math.floor(difference/1000/60)
+
+console.log(minutes);
+
+function zeit(timestring) {
     
-}
 
+let stunden = timestring[0] + timestring[1]
+let minuten = timestring[2] + timestring[3]
+let sekunden = timestring[4] + timestring[5]
+let date = new Date(2021, 5, 17, stunden, minuten, sekunden)
+console.log(date);
+return date
+}
